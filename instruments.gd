6507d8a652f2594c8265863ef28df7548de871c6
skooter500 <skooter500@gmail.com>
2026-01-26 22:31:17 +0000

    cont

--- instruments.gd
+++ instruments.gd
@@ -1,10 +1,10 @@
 extends Node3D
 
 func _ready() -> void:
-	sequencers.push_back($drums)
-	sequencers.push_back($sequencer)
-	sequencers.push_back($sequencer2)
-	sequencers.push_back($sequencer3)
+	sequencers.push_back($drums/pickable/sequencer)
+	sequencers.push_back($sequencer/pickable/sequencer)
+	sequencers.push_back($sequencer2/pickable/sequencer)
+	sequencers.push_back($sequencer3/pickable/sequencer)
 
 	
 
