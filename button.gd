de9313365d47c5fabc2ddc2b2e334617d47838d5
skooter500 <skooter500@gmail.com>
2026-01-31 20:02:29 +0000

    buttons

--- button.gd
+++ button.gd
@@ -3,6 +3,14 @@ extends Area3D
 var in_color:Color
 var out_color:Color
 
+enum BType {IN_OUT, TOGGLE}
+
+var btype:BType = BType.IN_OUT
+
+enum State {ON, OFF}
+
+var state = State.OFF
+
 func _ready() -> void:
 	in_color = Color.from_hsv(randf(), 1, 1, 0.5)
 	out_color = Color.from_hsv(fmod(in_color.h + 0.5, 1.0), 1, 1, 0.5)
@@ -14,12 +22,23 @@ func _ready() -> void:
 func _on_area_entered(area: Area3D) -> void:
 	if not area.is_in_group("finger_tip"):
 		return
-	$MeshInstance3D.get_surface_override_material(0).albedo_color = in_color 
+		
+	if btype == BType.IN_OUT:
+		$MeshInstance3D.get_surface_override_material(0).albedo_color = in_color 
+	elif btype == BType.TOGGLE:
+		if state == State.OFF:
+			state = State.ON
+			$MeshInstance3D.get_surface_override_material(0).albedo_color = in_color 
+		else:
+			state = State.OFF
+			$MeshInstance3D.get_surface_override_material(0).albedo_color = out_color 
+
 	pass # Replace with function body.
 
 
 func _on_area_exited(area: Area3D) -> void:
 	if not area.is_in_group("finger_tip"):
 		return
-	$MeshInstance3D.get_surface_override_material(0).albedo_color = out_color	
+	if btype == BType.IN_OUT:
+		$MeshInstance3D.get_surface_override_material(0).albedo_color = out_color	
 	pass # Replace with function body.
